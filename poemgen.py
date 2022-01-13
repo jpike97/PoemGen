cdda2e19117024daba822937f9cdcaaef7ad3cdd
jpike97 <jpike@sagepath.com>
2022-01-13 17:10:09 -0500

    refactoring a little bit

--- poemgen.py
+++ poemgen.py
@@ -5,65 +5,54 @@ import requests
 final_poem = []
 q = 0
 file_number = 0
-print ("Hello \n World")
+custom_poem_length = 5
 
-
-def scan_poem(poem):
-    poem_length = len(poem)
-    random_number = random.randint(1,poem_length)
-    z = 0
-    print (poem_number)
-
-
-
-    for x in poem: 
-        y = x.text.strip()
-        z = z + 1
-        if z == random_number & len(y) > 15:
-            scan_poem(poem)
-
-
+def stripIndividualPoem(poem):
+    poemLength = len(poem)
+    numLinesToTake = random.randint(2, 3)
+    lineCount = 0
+    arrayOfLines = []
+    for x in poem:
+        # print ("line is " + x)
+        if (lineCount == numLinesToTake):
+            ##print("returning")
+            ##print(arrayOfLines)
+            return arrayOfLines
         else:
-            if z == random_number:
-                return y
-
-while (file_number<20):
-    custom_poem_length = random.randint(5,15)
-    q = 0
-    while (q<int(custom_poem_length)):
-        print (custom_poem_length)
-        print (q)
-        baseurl = 'https://www.poetryfoundation.org/poems/'
-
-        poem_number = random.randint(45000,49000)
+            arrayOfLines.append(x)
+            lineCount += 1
 
 
-        complete_url = baseurl + str(poem_number)
+while (q<int(custom_poem_length)):
+    #print (custom_poem_length)
+    #print (q)
+    apiurl = 'https://poetrydb.org/random'
+    response = requests.get(apiurl)
+    poemResponse = response.json()
+    poem = poemResponse[0]["lines"]
+    if (len(poem) != 0):
+        ##print(stripIndividualPoem(poem))
+        final_poem.append(stripIndividualPoem(poem))
+        ##print("final poem is currently")
+        ##print (final_poem)
+        q+=1 
+    else:
+        print ("Crap")
 
-        page = requests.get(complete_url)
-
-        soup = BeautifulSoup(page.content, 'html.parser')
-
-        poem = soup.find_all('div', {"style" : "text-indent: -1em; padding-left: 1em;"})
-        if (len(poem) != 0):
-            final_poem.append(scan_poem(poem))
-            print (final_poem)
-            q = q+1
-        else:
-            print ("Crap")
-
-    file_name = "poem" + str(file_number) + ".txt"
-    f = open(file_name, "w")
-    for line in final_poem:
-        try:
-            f.write(line)
+file_name = "poemName.txt"
+f = open(file_name, "w")
+print ("final poem is ")
+print(final_poem)
+for line in final_poem:
+    try:
+        ##f.write(line)
+        for line_piece in line:
+            print ("line piece is " + line_piece)
+            f.write(line_piece)
             f.write("\n")
-        except:
-            f.write("")
-    
-    f.close()
-    file_number = file_number + 1
-    final_poem = []
+    except:
+        f.write("fail")
+f.close()
 
     
     
